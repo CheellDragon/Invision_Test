680b5fb45ffa2ed1eff640c3ee0415be8119324b
Нұржан Өскенбаев <nurzhan98n@gmail.com>
2023-03-20 10:59:23 +0600

    Добавил Loyalty блок для десктоп

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,41 @@
 import './Footer.css'
 
 const Footer = () => {
-    return (<></>);
+    return (
+        <div className='footer'>
+            <div className="container">
+                <div className="logo">
+                    <img src="./img/logo.png" alt="logo" />
+                </div>
+                <div className="nav">
+                    <a href="#a" className="nav_link">White paper</a>
+                    <a href="#a" className="nav_link">White List</a>
+                    <a href="#a" className="nav_link">Раунды пресейлов</a>
+                    <a href="#a" className="nav_link">Куср “Хакни трейдинг”</a>
+                    <a href="#a" className="nav_link">Лавка мерча и NFT</a>
+                    <a href="#a" className="nav_link">Функционал терминала Letit</a>
+                </div>
+                <div className="info">
+                    <a href="#a" className="nav_link">FAQ</a>
+                    <a href="#a" className="nav_link">Криптословарь</a>
+                    <a href="#a" className="nav_link">letit@letit.ap</a>
+                </div>
+                <div className="links">
+                    <div className="socials">
+                        <a href="#a" className="socials_link"><img className='socials_img' src="./img/Twitter.svg" alt="Twitter" /></a>
+                        <a href="#a" className="socials_link"><img className='socials_img' src="./img/Telegram.svg" alt="Telegram" /></a>
+                        <a href="#a" className="socials_link"><img className='socials_img' src="./img/Instagram.svg" alt="Instagram" /></a>
+                        <a href="#a" className="socials_link"><img className='socials_img' src="./img/Linkedln.svg" alt="Linkedln" /></a>
+                        <a href="#a" className="socials_link"><img className='socials_img' src="./img/Youtube.svg" alt="Youtube" /></a>
+                        <a href="#a" className="socials_link"><img className='socials_img' src="./img/Reddit.svg" alt="Reddit" /></a>
+                    </div>
+                    <div className="links_text">
+                        Подписывайтесь на открытые каналы Letit, каждый день что-то происходит
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
--- src/components/Loyalty/Loyalty.jsx
+++ src/components/Loyalty/Loyalty.jsx
@@ -20,8 +20,36 @@ const Loyalty = () => {
                 </div>
                 {
                     opened ?
-                    <div className="gridContainer">
-                        Some
+                    <div className="loyalty_opened">
+                        <div className="gridContainer">
+                            <div className="options"></div>
+                            <div className="options"><p className="options_text">5000+</p><img src="./img/Frame5000.png" alt="5000" /></div>
+                            <div className="options"><p className="options_text">10000+</p><img src="./img/Frame10000.png" alt="10000" /></div>
+                            <div className="options"><p className="options_text">20000+</p><img src="./img/Frame20000.png" alt="20000" /></div>
+                            <div className="options"><p className="options_text">50000+</p><img src="./img/Frame50000.png" alt="50000" /></div>
+                            <div className="programs">Brave</div>
+                            <div className="programs">-50%</div>
+                            <div className="programs cpec">-100%</div>
+                            <div className="programs cpec">-100%</div>
+                            <div className="programs cpec">-100%</div>
+                            <div className="programs">Clever</div>
+                            <div className="programs">-30%</div>
+                            <div className="programs">-50%</div>
+                            <div className="programs cpec">-100%</div>
+                            <div className="programs cpec">-100%</div>
+                            <div className="programs">Wise</div>
+                            <div className="programs">-10%</div>
+                            <div className="programs">-20%</div>
+                            <div className="programs">-50%</div>
+                            <div className="programs cpec">-100%</div>
+                            <img className='animatedFlower' src="./img/Flower.png" alt="flower" />
+                            <div className="understand">Форум</div>
+                            <div className="understand">без приоритета</div>
+                            <div className="understand">низкий</div>
+                            <div className="understand">средний</div>
+                            <div className="understand">высокий</div>
+                        </div>
+                        <div className="underText">Для держателей 5000 и выше токенов сервисы Letit предоставляется без рекламы!</div>
                     </div>
                     : null
                 }
